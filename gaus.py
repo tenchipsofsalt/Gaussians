bed5f66dff3bb7aa57825f49f94f8d1b1a488e89
Patrick Peng <44712940+tenchipsofsalt@users.noreply.github.com>
2022-04-02 20:03:40 -0400

    weee plotting

--- gaus.py
+++ gaus.py
@@ -1,7 +1,10 @@
 import math
 
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+
+sns.set_palette("viridis")
 
 
 def new_random_seq(s):
@@ -19,17 +22,17 @@ def calc_seq_cost(s):
     cost = 0
     for i in range(1, s.shape[0]):
         cost += np.power(s[i] - s[i - 1], 2)
-    return 1/2 * cost
+    return 1 / 2 * cost
 
 
 # number of RVs
-n = 5
+n = 100
 # number of samples to draw for each beta
 n_samples = 5000
 
 r = np.random.standard_normal(n)
 
-betas = [0, 100]
+betas = [0.01, 0.1, 1, 10, 100]
 mats = []
 
 for beta in betas:
@@ -42,9 +45,8 @@ for beta in betas:
 
         # calculate costs
         cost_delta = calc_seq_cost(proposed) - calc_seq_cost(seq)
-        # accept with probability relative to relative probabilities on pmf pB:
-        # if cost_delta < 0 or np.random.uniform(0, 1) < math.exp((- beta) * cost_delta):
-        # print(math.exp((- beta) * cost_delta))
+        # accept with probability relative to relative probabilities on pmf pB (note if delta is negative this will
+        # always accept):
         if np.random.uniform(0, 1) < math.exp((- beta) * cost_delta):
             seq = proposed
         r_s = np.expand_dims(np.multiply(r, seq), 1)
@@ -54,6 +56,18 @@ for beta in betas:
         # print(seq_cost * np.matmul(r_s, r_s.T))
 
     mat /= n_samples
-
-    print(mat)
     mats.append(mat)
+
+
+fig, axs = plt.subplots(1, len(betas))
+fig.suptitle("Transition from Diagonal to Full-Rank Matrix for Various Betas")
+fig.set_size_inches(12, 4)
+for i in range(len(betas)):
+    axs[i].imshow(mats[i], interpolation='none', label='beta: ' + str(betas[i]))
+    axs[i].axis('off')
+    axs[i].set_title('beta: ' + str(betas[i]))
+plt.show()
+
+# plot GRVs
+for mat in mats:
+
